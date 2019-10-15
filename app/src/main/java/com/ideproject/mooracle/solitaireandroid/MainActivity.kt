d65f2337732f6d038fe76863ad23d3d20661118f
mooracle <yanuar.prakosa@gmail.com>
2019-10-15 14:11:27 +0700

    Placing the Button

--- app/src/main/java/com/ideproject/mooracle/solitaireandroid/MainActivity.kt
+++ app/src/main/java/com/ideproject/mooracle/solitaireandroid/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.ideproject.mooracle.solitaireandroid
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.button
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.relativeLayout
-import org.jetbrains.anko.textView
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,8 +22,12 @@ class MainActivity : AppCompatActivity() {
                 text = "0"
                 //also set the properties of the text as in text size
                 textSize = 24f //f here denotes the number 24 is not an integer but a float
+                //set an id as reference for layout placement of the button
+                id = 10 //just pick any unique positive integer in the whole code environment.
             }
             button {
+                //set button text
+                text = "Click to Count"
                 //set onClick listner
                 onClick {
                     // when clicked increment the counter by 1
@@ -32,6 +35,8 @@ class MainActivity : AppCompatActivity() {
                     //set the counter text view to display the latest counter value
                     counterTextView.text = counter.toString()
                 }
+            }.lparams {
+                below(counterTextView)
             }
         }
     }
